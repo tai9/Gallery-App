1c9ce09bd89ffe6739d99dceee6756d9bc98d15e
tantai <tantai.it95@gmail.com>
2020-12-08 21:27:45 +0700

    remove file not usage

--- src/routers/authed.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Authed = () => {
-  return <div></div>;
-};
-
-export default Authed;
--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Home from "../components/Home/Home";
+// import Home from "../components/Home/Home";
 import Login from "../components/Login/Login";
 import SignUp from "../components/SignUp/SignUp";
 import Loading from "../pages/Loading/Loading";
