a9d5aa95a02e53497f0b035850d0284d2e6a2eda
tantai <tantai.it95@gmail.com>
2020-12-08 11:43:00 +0700

    add footer component

---
+++ src/components/Footer/Footer.styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  text-align: center;
+  padding: 10px 0;
+`;
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Wrapper } from "./Footer.styles";
+const Footer = () => {
+  return (
+    <Wrapper>
+      <p>Made with ❤ by Tai9.</p>
+    </Wrapper>
+  );
+};
+
+export default Footer;
